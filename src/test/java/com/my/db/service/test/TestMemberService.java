453a8b4f666a474b71392ed1a96151d4b438778e
羅智全 <96677946+lobenny139@users.noreply.github.com>
2024-04-06 13:16:13 +0800

    Create TestMemberService.java

---
+++ src/test/java/com/my/db/service/test/TestMemberService.java
@@ -0,0 +1,65 @@
+package com.my.db.service.test;
+
+import com.my.db.entity.Member;
+import com.my.db.service.IMemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Calendar;
+import java.util.List;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.my.db.TestApplication.class)
+@TestPropertySource(locations = "/test-application.properties")
+@AutoConfigureTestDatabase(replace = NONE)
+@Rollback(value = false)
+public class TestMemberService {
+
+    @Autowired(required = true)
+//    @Qualifier("memberService")
+    private IMemberService service;
+
+
+
+    @Test
+    public void testCreate() {
+        Member entity = new Member();
+        entity.setAccount("Benny");
+        entity.setPassword("1234");
+        entity.setIsActive(1);
+        entity.setName("Benny");
+        entity.setCreateBy("Admin");
+        entity.setCreateDate(Calendar.getInstance().getTime());
+        System.out.println(service.createEntity(entity));
+    }
+
+    @Test
+    public void testGetAll(){
+        List<Member> members = (List<Member>) service.getAllEntities();
+        for(Member member : members){
+            System.out.println(member);
+        }
+    }
+
+    @Test
+    public void testGetByAccount(){
+        Member member = service.getEntityByAccount("Benny1");
+        System.out.println(member);
+    }
+
+    @Test
+    public void testGetById(){
+        Member member = service.getEntityById(12L);
+        System.out.println(member);
+    }
+
+}
